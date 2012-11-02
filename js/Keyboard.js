e21265f0b0ed431de43d71f9459fdc3147b5fdf9
Kage1989 <kwok.hoe.89@gmail.com>
2012-11-02 22:33:08 +0800

    Update js/Keyboard.js
    Changed g_fuzzle.caty to g_fuzzle.posy

--- js/Keyboard.js
+++ js/Keyboard.js
@@ -4,22 +4,22 @@ function keyboard()
 	{
 	
 		//if player is within the game boundary defined by floorHeight and ceilingHeight
-		if ((g_fuzzle.caty + g_fuzzle.height) < 640 && g_fuzzle.caty > 10)
+		if ((g_fuzzle.posy + g_fuzzle.height) < 640 && g_fuzzle.posy > 10)
 		{
 			g_fuzzle.velocity = -10;
-			g_fuzzle.caty += g_fuzzle.velocity;
+			g_fuzzle.posy += g_fuzzle.velocity;
 		}
 		//if player crashes into the floor
-		if ((g_fuzzle.caty + g_fuzzle.height) > 640)
+		if ((g_fuzzle.posy + g_fuzzle.height) > 640)
 		{
-			g_fuzzle.caty = (640 - g_fuzzle.height - 0.5);
+			g_fuzzle.posy = (640 - g_fuzzle.height - 0.5);
 			g_fuzzle.velocity = -10;
-			g_fuzzle.caty += g_fuzzle.velocity;
+			g_fuzzle.posy += g_fuzzle.velocity;
 		}
 		//if player crashes into the ceiling
-		if (g_fuzzle.caty < 0)
+		if (g_fuzzle.posy < 0)
 		{
-			g_fuzzle.caty = 0 + 0.1;
+			g_fuzzle.posy = 0 + 0.1;
 			g_fuzzle.velocity = 0;
 		}
 	}
