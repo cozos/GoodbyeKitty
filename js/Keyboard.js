4d8c9e71797424186d86a54f862b9f2a7972023a
Kage1989 <kwok.hoe.89@gmail.com>
2012-11-02 21:27:50 +0800

    Update js/Keyboard.js

--- js/Keyboard.js
+++ js/Keyboard.js
@@ -1,19 +1,30 @@
-function KEY(e)
+function keyboard()
 {
+	if (g_keys[38] == true) //check if up arrow is pressed
+	{
 	
-   //dbg(e.keyCode + "down", false);
+		//if player is within the game boundary defined by floorHeight and ceilingHeight
+		if ((g_fuzzle.caty + g_fuzzle.height) < 640 && g_fuzzle.caty > 10)
+		{
+			g_fuzzle.velocity = -10;
+			g_fuzzle.caty += g_fuzzle.velocity;
+		}
+		//if player crashes into the floor
+		if ((g_fuzzle.caty + g_fuzzle.height) > 640)
+		{
+			g_fuzzle.caty = (640 - g_fuzzle.height - 0.5);
+			g_fuzzle.velocity = -10;
+			g_fuzzle.caty += g_fuzzle.velocity;
+		}
+		//if player crashes into the ceiling
+		if (g_fuzzle.caty < 0)
+		{
+			g_fuzzle.caty = 0 + 0.1;
+			g_fuzzle.velocity = 0;
+		}
+	}
+	if (g_keys[40] == true) //check if down arrow is pressed
+	{
 
-
-
-   if ( e.keyCode == 38 )              // up arrow
-   {
-   		
-      g_fuzzle.up();
-      
-   }
-   else if ( e.keyCode == 40 )         // down arrow
-   {
-      g_fuzzle.down();
-   }
-
+	}
 }
